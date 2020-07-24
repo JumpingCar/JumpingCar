26099b2c9338df0242602f7251a721d6c97f9ae0
Sunjuhyeong <munuwazzi@kaist.ac.kr>
2020-07-24 11:54:50 +0900

    add Crossover

--- neuralnetwork.js
+++ neuralnetwork.js
@@ -42,7 +42,7 @@ class NeuralNetwork {
 
     importGenes(genes) {
         let count = 0
-
+        
         for (let i = 0; i < this.weights.length; i++) {
             this.weights[i].construct(genes.slice(count, count + this.weights[i].row * this.weights[i].column))
             count += this.weights[i].row * this.weights[i].column
@@ -53,4 +53,78 @@ class NeuralNetwork {
             count += this.bias[i].row * this.bias[i].column
         }
     }
+
+    static crossover(nn1, nn2) {
+        const parentGene1 = nn1.exportGenes()
+        const parentGene2 = nn2.exportGenes()
+        if (parentGene1.length != parentGene2.length)
+            throw new Error("Wrong Arity")
+        
+        let children = []
+        
+
+        //Parent : 2 new children
+        children = children.concat([parentGene1, parentGene2])
+        
+        //1 point Crossover : 2 new children
+        let child1 = []
+        let child2 = []
+        let pivot1 = Math.floor(Math.random() * parentGene1.length)
+
+        for (let i = 0; i < pivot; i++) {
+            child1.push(parentGene1[i])
+            child2.push(parentGene2[i])
+        }
+        for (let i = pivot; i < parentGene1.length; i++) {
+            child1.push(parentGene2[i])
+            child2.push(parentGene1[i])
+        }
+
+        children = children.concat([child1, child2])
+
+        //2 point Crossover : 2 new children
+        let child1 = []
+        let child2 = []
+        let pivot1 = Math.floor(Math.random() * parentGene1.length)
+        let pivot2 = Math.floor(Math.random() * parentGene1.length)
+
+        for (let i = 0; i < Math.min(pivot1, pivot2); i++) {
+            child1.push(parentGene1[i])
+            child2.push(parentGene2[i])
+        }
+        for (let i = min(pivot1, pivot2); i < max(pivot1, pivot2); i++) {
+            child1.push(parentGene2[i])
+            child2.push(parentGene1[i])
+        }
+        for (let i = max(pivot1, pivot2); i < parentGene1.length; i++) {
+            child1.push(parentGene1[i])
+            child2.push(parentGene2[i])
+        }
+
+        children = children.concat([child1, child2])
+
+        //uniform Crossover : 2 new children
+        let child1 = []
+        let child2 = []
+        let change = []
+        for (let i = 0; i < parentGene1.length; i++) {
+            change.push(Math.floor(Math.random()*2)) // 'change' is array of 0 or 1.
+        }
+
+        for (let i = 0; i < parentGene1.length; i++) {
+            if (change[i] == 0) {
+                child1.push(parentGene1[i])
+                child2.push(parentGene2[i])
+            } else {
+                child1.push(parentGene2[i])
+                child2.push(parentGene1[i])
+            }
+        }
+            
+        children = children.concat([child1, child2])
+
+
+        return children // total 8 new children
+    }
+
 }
