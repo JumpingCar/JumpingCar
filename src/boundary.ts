11ec8e6dea6f6ed9603d85304e6bd4918ad022c0
pacokwon <haechank@gmail.com>
2020-07-25 13:07:20 +0900

    update: fix editorconfig and apply settings to original files

--- src/boundary.ts
+++ src/boundary.ts
@@ -11,14 +11,14 @@ export class Boundary {
     b:p5.Vector
 
     constructor(p:p5, x1:number, y1:number, x2:number, y2:number) {
-      this.a = p.createVector(x1, y1);
-      this.b = p.createVector(x2, y2);
+        this.a = p.createVector(x1, y1);
+        this.b = p.createVector(x2, y2);
     }
-  
+
     show(p:p5) {
-      p.stroke(255);
-      p.strokeWeight(3);
-      p.line(this.a.x, this.a.y, this.b.x, this.b.y);
+        p.stroke(255);
+        p.strokeWeight(3);
+        p.line(this.a.x, this.a.y, this.b.x, this.b.y);
     }
-  }
-  
+}
+
