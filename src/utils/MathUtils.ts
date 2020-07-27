b749b7f5ef2abb3b2c9386df972ef79c730843d3
pacokwon <haechank@gmail.com>
2020-07-27 21:52:19 +0900

    add: dot to line distance in MathUtils

--- src/utils/MathUtils.ts
+++ src/utils/MathUtils.ts
@@ -7,4 +7,10 @@ export default class MathUtils {
         const crossed = atob.cross(atoc)
         return Math.abs(crossed.z) / 2
     }
+
+    // (x1, y1) - (x2, y2): line | (x3, y3): dot
+    public static lineDotDistance(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number) {
+        return Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - x2 * y1 - x3 * y2 - x1 * y3) / Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+    }
+
 }
